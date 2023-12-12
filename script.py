bf14ecb4717bbbcbe5bd2e3ad7d5e0702154bbae
Territum <39955043+dputh@users.noreply.github.com>
2023-12-12 16:13:33 +0100

    hmmm

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
-with open('chickens.txt', 'w') as file:
+with open('/home/chickens/shared_folder/chickens.txt', 'w') as file:
     # Write some text to the file
     file.write('chickens are the strongest creatures in the animal kingdom')
+    print(file)
