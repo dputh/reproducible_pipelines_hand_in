1ad3032d690b5095567cf61341141513038df91a
Territum <39955043+dputh@users.noreply.github.com>
2023-12-13 23:44:05 +0100

    change shared folder path

--- functions.R
+++ functions.R
@@ -9,5 +9,5 @@ make_plot <-function(data){
     geom_histogram(bins = 30, position = 'identity', alpha = 0.6) +
     labs(title = 'Distribution of Heights by Gender', x = 'Height', y = 'Count') +
     theme_minimal()
-  ggsave('/home/graphs/height_distribution.png', plot = p, width = 10, height = 6)
+  ggsave('/home/graphs/home/graphs/shared_folder/height_distribution.png', plot = p, width = 10, height = 6)
 }
